fba53db8567b5bb5c85c52e865379618eb83e018
Bryan Camilo Pelaez <150277380+BryanCPl@users.noreply.github.com>
2026-08-13 21:39:35 -0500

    Inital commit

---
+++ GaussGame.py
@@ -0,0 +1,140 @@
+class gaussGame:
+    #They are organized to avoid conflicts
+    OPN=["<->","*","/","+","-"]
+    table=[]
+    def __init__(self) -> None:
+        self.matrixIdentity=[[1,0,0],[0,1,0],[0,0,1]]
+
+    def setTable(self,table):
+        self.table=table
+
+    def showTable(self):
+        text=f"{str(self.table[0])} \n {str(self.table[1])} \n {str(self.table[2])}"
+        return text
+        
+
+    def showMatrixID(self):
+        for id in self.matrixIdentity:
+                print(id)
+
+
+    def substracRow(self,m1,m2,r1,r2):
+        result=[]
+        table2=self.RowOrNum(m2,r2)
+        for i in [0,1,2,3]:
+            result.append(m1*self.table[r1][i]-table2[i])
+        self.table[r1]=result
+        
+        
+
+    def AddRow(self,m1,m2,r1,r2):
+            result=[]
+
+            table2=self.RowOrNum(m2,r2)
+            for i in [0,1,2,3]:
+                print(result)
+                result.append(m1*self.table[r1][i]+table2[i])
+            
+            self.table[r1]=result
+
+
+    def MultRows(self,m1,m2,r1,r2):
+            result=[]
+            table2=self.RowOrNum(m2,r2)
+            for i in [0,1,2,3]:
+                result.append(m1*self.table[r1][i]+table2[i])
+            self.table[r1]=result
+
+    def DivRows(self,m1,m2,r1,r2):
+                result=[]
+                table2=self.RowOrNum(m2,r2)
+                for i in [0,1,2,3]:
+                    #this is only for dont show negative 0
+                    if self.table[r1][i]!=0:
+                        result.append(m1*self.table[r1][i]/table2[i])
+                    else:
+                        result.append(m1*self.table[r1][i])
+                self.table[r1]=result
+
+    def RowManipulation(self,m1,m2,r1,r2,form):
+        result=[]
+        table2=self.RowOrNum(m2,r2)
+        for i in [0,1,2,3]:
+            result.append(form)
+        self.table[r1]=result
+
+    def RowOrNum(self,m2,r2):
+        if r2==None:
+            return [m2,m2,m2,m2]
+        else:
+            result=[]
+            for i in self.table[r2]:
+                result.append(m2*i)
+            return result
+
+    def chageRows(self,r1,r2):
+        result=self.table[r1]
+        self.table[r1]=self.table[r2]
+        self.table[r2]=result
+
+    def defineOperation(self,simbol:str,m1:int,m2:int,r1:int,r2:int):
+        print(r1)
+        match simbol:
+            case "+":
+                self.AddRow(m1,m2,r1,r2)
+            case "-":
+                self.substracRow(m1,m2,r1,r2)
+            case "*":
+                self.MultRows(m1,m2,r1,r2)
+            case "/":
+                self.DivRows(m1,m2,r1,r2)
+            case "<->":
+                self.chageRows(r1,r2)
+            case _:
+                return "only accept +,-,*,/"
+
+        self.showTable()
+
+    def operateRows(self,notation:str):
+        r1=None
+        r2=None
+        m1=1
+        m2=1
+        operation=""
+        try:
+
+            operation=self.whichOperation(notation)
+
+            comand=notation.split(operation)
+
+            r1=int(comand[0][-1])-1
+
+            if comand[0][0]!="f":
+                m1=comand[0][0]
+
+            #if has letter f want manipulate row
+            if "f" in comand[1]:
+                if comand[1][0]=="f":
+                    r2=int(comand[1][-1])-1
+                elif comand[1][1]=="f":
+                    r2=int(comand[1][-1])-1
+                    m2=int(comand[1][0])
+                #If f is in second place, behind it is the multiplier
+            else:
+                # only get the number
+                m2=int(comand[1])
+
+            print(f"{m1}f{r1}{operation}{m2}f{r2}")
+
+
+            self.defineOperation(operation,int(m1),int(m2),r1,r2) # type: ignore
+            return self.showTable()
+        except:
+            return "El comando no es valido prueba de nuevo"
+
+
+    def whichOperation(self,n):
+        for o in self.OPN:
+            if o in n:
+                return o
+        return "Error,No valid operation"
---
+++ GaussTable.py
@@ -0,0 +1,37 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+#from ..Models.GaussGame import gaussGame
+
+
+operation=['x','y','z','=']
+
+def makeTable(table):
+    return [separeateRow(table[0]),separeateRow(table[1]),separeateRow(table[2])]
+
+def separeateRow(formula):
+    try:
+        row=[formula[0:formula.find('x')],
+            formula[formula.find('x')+1:formula.find('y')],
+            formula[formula.find('y')+1:formula.find('z')],
+            formula[formula.find('=')+1:len(formula)]
+            ]
+        
+        print(row)
+        gaussRow=[]
+        for n in row:
+            posOrneg=1
+            if len(n)>0 and n[0]=="-":
+                posOrneg=-1
+
+
+            if n=='' or n=="+" or n=='-':
+                gaussRow.append(posOrneg*1)
+            elif n.isdigit():
+                gaussRow.append(int(n))
+            elif n[1:len(n)].isdigit():
+                gaussRow.append(posOrneg*int(n[1:len(n)]))
+        return gaussRow
+    except:
+        return "La formula no es validad"
+
+
